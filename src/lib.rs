2c8fe1a807d6bc1f73d1eb9da2421b942d7619b6
Chriszzz <chrisz2zz@duck.com>
2024-09-02 10:10:58 +0800

    feat: week1 finished & add rbtree basic implement;     1.为什么 memtable 不提供删除 API？     Re:我理解删除的话可能会涉及一些缩容问题,而且对于一些已经进入immtable的数据来说,删除成本很大,使用空值反而可以利用顺序写的优势提高性能     2.是否可以使用其他数据结构作为 LSM 中的 memtable？使用 skiplist 的优缺点是什么？     Re:使用其他数据结构当然可以,但是从实现成本来说skiplist更好,并且性能相当     3.为什么我们需要 state 和 state_lock 的组合？我们只能使用 state.read（） 和 state.write（） 吗？     Re:只使用state的话,加write锁会导致freeze时,无法读取数据,并发性较低     4.为什么存储和探测 memtable 的顺序很重要？如果一个 key 出现在多个 memtable 中，你应该将哪个版本返回给用户？     Re:因为整体的数据是一个顺序追加的方式,最新的在最上面,可以利用顺序写的优势;应该返回最新的版本     5.memtable 的内存布局是否有效/是否具有良好的数据局部性？（想想 skiplist 中是如何 Byte 实现和存储的......有哪些可能的优化可以提高 memtable 的效率？     Re:大概看了下skiplist源码,使用了一个cache line保存了比较热的数据,应该是遵循的LRU     6.因此，我们在本教程中使用 parking_lot 了锁。它的读写锁是公平的锁吗？如果有一个写入器正在等待现有读取器停止，那么尝 试获取锁的读取器会发生什么情况？     Re:是公平锁,源码注释有写,既然是公平锁,写入器等待读取器时,尝试获取读取锁不会获得锁,而是等待写入器释放后再获取     7.冻结 memtable 后，是否有可能一些线程仍然保持旧的 LSM 状态并写入这些不可变的 memtable？您的解决方案如何防止它发生？     Re:我理解是有可能的,需要拿到锁后进行二次校验     8.在几个地方，你可以先获取状态的读锁，然后删除它并获取写锁（这两个操作可能在不同的函数中，但由于一个函数调用另一个函数，它们按顺序发生）。它与直接将读锁升级为写锁有什么不同？是否需要升级而不是获取和放弃，进行升级的成本是多少？     Re:不太确定,gpt4之后,读锁升级写锁可能会死锁,多个读锁可能都在等待对方释放后再升级

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+pub mod block;
+pub mod compact;
+pub mod debug;
+pub mod iterators;
+pub mod key;
+pub mod lsm_iterator;
+pub mod lsm_storage;
+pub mod manifest;
+pub mod mem_table;
+pub mod mem_table_rbtree;
+pub mod mvcc;
+pub mod table;
+pub mod wal;
+
+#[cfg(test)]
+mod tests;
